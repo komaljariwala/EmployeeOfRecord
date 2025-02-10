b884d883bcfe08e6cbd149a504750494c0886602
komal jariwala <komalj2104@gmail.com>
2025-02-10 13:49:20 +0530

    update

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss']
+    templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
     // Add any necessary properties or methods here
