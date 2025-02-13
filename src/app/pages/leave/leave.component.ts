fe8106bfb2cf03f826790ce53e0b8d0661152ea1
jariwbh <jariwbh@gmail.com>
2025-02-13 14:47:55 +0530

    Initial commit

--- src/app/pages/leave/leave.component.ts
+++ src/app/pages/leave/leave.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-leave',
-  templateUrl: './leave.component.html'
+  templateUrl: './leave.component.html',
+  styleUrls: ['./leave.component.scss']
 })
 export class LeaveComponent implements OnInit {
-  leaveList = [
-    { type: 'Sick Leave', status: 'Approved' },
-    { type: 'Casual Leave', status: 'Pending' },
-    { type: 'Maternity Leave', status: 'Approved' }
-  ];
+  activeTab: string = 'request';
+  leaveRequestForm: FormGroup;
+  leaveTypes: string[] = ['Sick Leave', 'Casual Leave', 'Annual Leave'];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.leaveRequestForm = this.fb.group({
+      leaveType: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      reason: ['', Validators.required]
+    });
+  }
 
-  ngOnInit(): void {
-    // Initialization logic here
+  ngOnInit(): void {}
+
+  selectTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+  submitLeaveRequest() {
+    if (this.leaveRequestForm.valid) {
+      console.log('Leave Request Data:', this.leaveRequestForm.value);
+      alert('Leave request submitted successfully!');
+      this.leaveRequestForm.reset();
+    } else {
+      alert('Please fill all the required fields.');
+    }
   }
 }
--- src/app/pages/leave/leave.module.ts
+++ src/app/pages/leave/leave.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LeaveComponent } from './leave.component';
 import { LeaveRoutes } from './leave.routing';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LeaveComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(LeaveRoutes),
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LeaveModule { }
